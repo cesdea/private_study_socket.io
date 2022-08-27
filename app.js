6b0ebfe31a2a7a74ef3ee9335de59ea17e2f1b06
cesdea <aprkvnem@gmail.com>
2022-08-27 10:59:37 +0900

    update class server

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 var app = require('express')();
 const cors=require("cors")
 // http server를 socket.io server로 upgrade한다
-var route=require("./route")
+var server=require("./route")
 
+server.listen(3000, function() {
+  console.log('Socket IO server listening on port 3000');
+});
 // localhost:3000으로 서버에 접속하면 클라이언트로 index.html을 전송한다
 app.use(cors())
 app.get('/', function(req, res) {
