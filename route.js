9c721909c4ff33079723fc3b55aa9ce781dfd5c9
cesdea <aprkvnem@gmail.com>
2022-08-27 11:05:49 +0900

    error edit and restore

--- route.js
+++ route.js
@@ -1,6 +1,7 @@
 var app = require('express')();
 const cors=require("cors")
 const http=require('http')
+const socket=require('socket.io')
 // http server를 socket.io server로 upgrade한다
 
 // localhost:3000으로 서버에 접속하면 클라이언트로 index.html을 전송한다
@@ -9,17 +10,12 @@ const http=require('http')
 class Server{
   constructor() {
     this.server = http.createServer(app);
-    this.io = require('socket.io')(server);
+    this.io = socket(this.server);
     // this.setErrorHandler();
-  }
-  setMiddleWare(){
-    this.server.use(cors())
-  }
-  setRouter() {
-    this.io.on('connection', function(socket) {
+    this.io.on('connection', (socket)=> {
     
       // 접속한 클라이언트의 정보가 수신되면
-      socket.on('login', function(data) {
+      socket.on('login', (data)=> {
         console.log('Client logged-in:\n name:' + data.name + '\n userid: ' + data.userid);
     
         // socket에 클라이언트 정보를 저장한다
@@ -27,11 +23,11 @@ class Server{
         socket.userid = data.userid;
     
         // 접속된 모든 클라이언트에게 메시지를 전송한다
-        io.emit('login', data.name );
+        this.io.emit('login', data.name );
       });
     
       // 클라이언트로부터의 메시지가 수신되면
-      socket.on('chat', function(data) {
+      socket.on('chat', (data)=> {
         console.log('Message from %s: %s', socket.name, data.msg);
     
         var msg = {
@@ -43,7 +39,7 @@ class Server{
         };
     
         // 메시지를 전송한 클라이언트를 제외한 모든 클라이언트에게 메시지를 전송한다
-        io.emit('chat', msg);
+        this.io.emit('chat', msg);
     
         // 메시지를 전송한 클라이언트에게만 메시지를 전송한다
         // socket.emit('s2c chat', msg);
@@ -55,14 +51,19 @@ class Server{
         // io.to(id).emit('s2c chat', data);
       });
       // force client disconnect from server
-      socket.on('forceDisconnect', function() {
+      socket.on('forceDisconnect', ()=> {
         socket.disconnect();
       })
     
-      socket.on('disconnect', function() {
+      socket.on('disconnect', ()=> {
         console.log('user disconnected: ' + socket.name);
       });
     });
+  }
+  setMiddleWare(){
+    this.server.use(cors())
+  }
+  setRouter() {
 
   }
   
