b09fdf9e80f3f2ad8e08a4baf8250a0f5216a8c2
cesdea <aprkvnem@gmail.com>
2022-08-27 10:42:48 +0900

    test

--- app.js
+++ app.js
@@ -3,9 +3,9 @@ var app = require('express')();
 var route=require("./route")
 
 // localhost:3000으로 서버에 접속하면 클라이언트로 index.html을 전송한다
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+app.get('/', function(req, res) {
+  res.sendFile(__dirname + '/index.html');
+});
 app.listen(8000, () => {
   console.log(8000, '/api로 진행해주세요');
 });
